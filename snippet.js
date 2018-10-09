fdea4958cd03acb13dabcfceb0a28836d4b8dd62
Luis Ricardo <LuisRADias@users.noreply.github.com>
2018-10-09 12:52:02 -0300

    Update snippet.js

--- snippet.js
+++ snippet.js
@@ -1,3 +1,13 @@
+var lista_clientes = [];
+
 $(".fc-content-skeleton").find("td:nth-child(2)").find(".fc-event-container").find("a").each(function () {
-	console.log($(this).find(".fc-content").find(".fc-title").html().split(" - "));
+	lista_clientes.push($(this).find(".fc-content").find(".fc-title").html().split(" - "));
 });
+
+lista_clientes.forEach(function(rowArray){
+   let row = rowArray.join(",");
+   csvContent += row + "\r\n";
+});
+
+var encodedUri = encodeURI(csvContent);
+window.open(encodedUri);
