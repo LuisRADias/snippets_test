45e4d9d083a980f29d2f95b533ac91a8ed8e923c
Luis Ricardo <LuisRADias@users.noreply.github.com>
2018-10-09 14:00:45 -0300

    Create exportar_agenda_segunda.js

---
+++ exportar_agenda_segunda.js
@@ -0,0 +1,23 @@
+var lista_clientes = [];
+
+var dia = 2;
+
+$(".fc-content-skeleton").find("td:nth-child(" + dia + ")").find(".fc-event-container").find("a").each(function () {
+	lista_clientes.push($(this).find(".fc-content").find(".fc-title").html().split(" - "));
+});
+
+var lineArray = [];
+lista_clientes.forEach(function (infoArray, index) {
+    var line = infoArray.join(";");
+    lineArray.push(index == 0 ? "data:text/csv;charset=utf-8," + line : line);
+});
+var csvContent = lineArray.join("\n");
+
+var encodedUri = encodeURI(csvContent);
+var link = document.createElement("a");
+link.setAttribute("href", encodedUri);
+link.setAttribute("download", "agenda_dia.csv");
+link.innerHTML= "Click Here to download";
+document.body.appendChild(link); // Required for FF
+
+link.click(); // This will download the data file named "my_data.csv".
