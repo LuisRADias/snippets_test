4a848d1e8b6074cf8d9fe9d2278b1f33819485cf
Luis Ricardo <LuisRADias@users.noreply.github.com>
2018-10-09 13:46:36 -0300

    Update snippet.js

--- snippet.js
+++ snippet.js
@@ -4,11 +4,18 @@ $(".fc-content-skeleton").find("td:nth-child(2)").find(".fc-event-container").fi
 	lista_clientes.push($(this).find(".fc-content").find(".fc-title").html().split(" - "));
 });
 
-var csvContent = "";
-lista_clientes.forEach(function(rowArray){
-   let row = rowArray.join(",");
-   csvContent += row + "\r\n";
+var lineArray = [];
+lista_clientes.forEach(function (infoArray, index) {
+    var line = infoArray.join(";");
+    lineArray.push(index == 0 ? "data:text/csv;charset=utf-8," + line : line);
 });
+var csvContent = lineArray.join("\n");
 
 var encodedUri = encodeURI(csvContent);
-window.open(encodedUri);
+var link = document.createElement("a");
+link.setAttribute("href", encodedUri);
+link.setAttribute("download", "agenda_dia.csv");
+link.innerHTML= "Click Here to download";
+document.body.appendChild(link); // Required for FF
+
+link.click(); // This will download the data file named "my_data.csv".
